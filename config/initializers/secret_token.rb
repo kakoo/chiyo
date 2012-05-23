c54b2b025ce47370939c2806ecd6b11adf14afce
SunHee Park <kakoo47@gmail.com>
2012-05-23 20:53:33 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chiyo::Application.config.secret_token = 'b8cfc6ef3f216318321fd8d974bb670571510f159ad5fc0e5ecdd6a929aaf5a770e2759e997507b0fca78fb742ccdb3a3924a0960b6e9cade33d7341cc236233'
