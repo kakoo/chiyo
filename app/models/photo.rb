b7f359e8eacb626669a3219e47aadb00ae52c728
Sunhee Park <kakoo47@gmail.com>
2012-09-12 21:38:00 +0900

    저장된 flickr photo id 가져오도록.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -12,7 +12,7 @@ class Photo < ActiveRecord::Base
   before_create do |r|
     begin
       r.flickr_photo_id = r.user.flickr.upload_photo r.image.tempfile, :title => r.title
-      r.user.flickr.photosets.addPhoto :photoset_id => "72157630312659872", :photo_id => r.flickr_photo_id
+      r.user.flickr.photosets.addPhoto :photoset_id => r.user.flickr_photoset_id, :photo_id => r.flickr_photo_id
     rescue Exception => e
       false
     end
@@ -23,7 +23,7 @@ class Photo < ActiveRecord::Base
       r.user.flickr.photos.delete :photo_id => r.flickr_photo_id
 
       r.flickr_photo_id = r.flickr.upload_photo r.image.tempfile, :title => r.title
-      r.flickr.photosets.addPhoto :photoset_id => "72157630312659872", :photo_id => r.flickr_photo_id
+      r.flickr.photosets.addPhoto :photoset_id => r.user.flickr_photoset_id, :photo_id => r.flickr_photo_id
     end
   end
 end
